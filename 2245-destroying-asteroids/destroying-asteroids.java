a18321cc0682fe9510061d43a7b16d57f15465c0
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-05-31 14:37:53 +0530

    Time: 26 ms (81.52%) | Memory: 107.2 MB (84.30%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        long total =mass;
+        for(int i=0;i<asteroids.length;i++){
+            if(asteroids[i]>total){
+                return false;
+            }
+            total+=asteroids[i];
+        }
+        return true;
+
+    }
+}
