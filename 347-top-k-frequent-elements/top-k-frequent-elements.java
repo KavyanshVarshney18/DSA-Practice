a1915e40d9a66d7803b39e10f2188e219748b433
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-07-22 23:00:08 +0530

    Time: 13 ms (79.93%) | Memory: 47.5 MB (82.11%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,3 +1,19 @@
+class Pair implements Comparable<Pair>{
+    int num;
+    int freq;
+
+    Pair(int num,int freq){
+        this.num = num;
+        this.freq = freq;
+    }
+
+    public int compareTo(Pair p){
+        if(this.freq == p.freq) return Integer.compare(this.num , p.num);
+
+        return Integer.compare(this.freq,p.freq);
+    }
+}
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -5,17 +21,21 @@ class Solution {
             map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
         }
 
-        PriorityQueue<Map.Entry<Integer,Integer>> pq = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
-
-
-        pq.addAll(map.entrySet());
-
-        int[] ans = new int[k];
-        for(int i=0;i<k;i++){   
-            ans[i] = pq.poll().getKey();
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int ele : map.keySet()){
+            int freq = map.get(ele);
+            pq.add(new Pair(ele , freq));
+            if(pq.size()>k){
+                pq.remove();
+            }
         }
-
+        int[] ans = new int[pq.size()];
+        int idx=0;
+        while(pq.size()>0){
+            Pair top = pq.remove();
+            int val = top.num;
+            ans[idx++] = val;
+        }
         return ans;
     }
-
 }
