623ea56faa7f36355346ed3d6c82043ceb46ff9b
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-07-27 00:38:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (78.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,32 +1,45 @@
 class Solution {
     public int rob(int[] nums) {
-       int n = nums.length;
-       if(n==0){
-           return 0;
-       }
-       if(n==1){
-           return nums[0];
-       }
-       if(n==2){
-        return Math.max(nums[0],nums[1]);
-       }
-       
-       int[] dp = new int[n];
-       int[] dp1 = new int[n];
+        int n = nums.length;
+        if(n == 1) return nums[0];
 
-       dp[0] = nums[0];
-       dp[1] = Math.max(dp[0],nums[1]);
-       dp1[0] = nums[1];
-       dp1[1] = Math.max(nums[1],nums[2]);
-       
-       for(int i=2;i<n-1;i++){
-           dp[i] = Math.max( (nums[i]+dp[i-2]) , (dp[i-1]));
-       }
-        for(int i=2;i<=n-2;i++){
-           dp1[i] = Math.max( (nums[i+1]+dp1[i-2]) , (dp1[i-1]));
-       }
-      
-       return Math.max(dp[n-2],dp1[n-2]);
-        
+
+        int[] skipfirsthouse = new int[n-1];
+        int[] skiplasthouse = new int[n-1];
+
+        for(int i=1;i<n;i++){
+            skipfirsthouse[i-1] = nums[i];
+        }
+        for(int i=0;i<n-1;i++){
+            skiplasthouse[i] = nums[i];
+        }
+
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1,-1);
+
+        int[] dp2 = new int[n-1];
+        Arrays.fill(dp2,-1);
+
+        int maxfromfirst = rob1( skipfirsthouse, n-2 , dp1);
+        int maxfromsecond = rob1( skiplasthouse, n-2 , dp2);
+
+        return Math.max(maxfromfirst , maxfromsecond);
+
+    }
+
+
+    public int rob1(int[] nums , int n , int[] dp){
+        if(n<0) return 0;
+        if(n==0) return nums[0];
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int include = rob1(nums , n-2 , dp) + nums[n];
+        int exclude = rob1(nums , n-1 , dp) + 0;
+
+        dp[n] = Math.max(include , exclude);
+        return dp[n];
     }
 }
