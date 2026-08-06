4ee4e61fdf6928af44321d6f90d73da5487635cb
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-08-06 22:13:47 +0530

    Time: 148 ms (8.87%), Space: 202 MB (5.01%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        Integer[][][] dp = new Integer[n][2][3];
+
+        return helper(prices , 0 , 1 , 2,dp);  //here 2 is the max capacity , 0 -> index , 1 -> can buy
+    }
+
+
+
+
+    public int helper(int[] prices , int i , int buy , int capacity , Integer[][][] dp){
+        if(capacity==0){
+            return 0;
+        }
+
+        if(i==prices.length){
+            return 0;
+        }
+
+        if(dp[i][buy][capacity]!=null){
+            return dp[i][buy][capacity];
+        }
+
+        int profit = 0;
+
+        if(buy==1){
+            int buykaro = -prices[i] + helper(prices , i+1 , 0 , capacity ,dp); // capacity will be same bc i have not sell it till now
+
+            int skip = helper(prices , i+1 ,1 ,capacity ,dp);
+
+            profit = Math.max(buykaro , skip); 
+        }
+
+        else{
+            int sell  = prices[i] + helper(prices , i+1 , 1 , capacity-1 ,dp);
+
+            int skip = helper(prices , i+1 , 0 , capacity,dp);
+
+            profit = Math.max(sell ,skip);
+        }
+
+        return dp[i][buy][capacity] = profit;
+
+
+    }
+}
