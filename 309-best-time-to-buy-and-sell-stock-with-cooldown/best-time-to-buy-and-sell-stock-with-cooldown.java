94a744f082479b04e6ea60c883788daf135803e2
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-08-07 00:55:11 +0530

    Time: 1 ms (69.99%) | Memory: 43.7 MB (19.71%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        Integer[][] dp = new Integer[n][2];   //n->days , 2-> take , nottake max
+        return helper(prices , 1 , 0 ,dp);   //1 means i can buy initially , 0 is the index
+    }
+    public int helper(int[] prices , int buy , int i,Integer[][] dp){
+        if(i>=prices.length){
+            return 0;
+        }
+        if(dp[i][buy]!=null) {
+            return dp[i][buy];
+        }
+
+        int profit =0;
+        if(buy==1){
+            int buykaro = -prices[i] + helper(prices , 0 ,i+1 ,dp);
+            int skipkaro = helper(prices , 1,i+1 ,dp);
+            profit = Math.max(buykaro , skipkaro);
+        }
+
+        else{
+            int sellkaro = prices[i] + helper(prices , 1 , i+2 ,dp);
+            int skipkaro = helper(prices , 0,i+1 ,dp);
+            profit = Math.max(sellkaro , skipkaro);
+        }
+
+        return dp[i][buy] = profit;
+    }
+}
