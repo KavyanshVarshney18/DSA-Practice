4f3adedd92b5c76f2015d90eff7c49dc25583b70
Kavyansh Varshney <147943829+KavyanshVarshney18@users.noreply.github.com>
2026-09-08 22:11:17 +0530

    Time: 359 ms (5.01%) | Memory: 48.1 MB (23.41%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,24 +1,27 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp , -1);
-        return helper(nums , 0 , dp);
+        Boolean[] dp = new Boolean[nums.length];
+        return helper(nums,0,dp);
     }
-    public boolean helper(int[] nums , int i , int[] dp){
+
+
+    public boolean helper(int[] nums , int i,Boolean[] dp){
         if(i==nums.length-1){
             return true;
         }
-        if(dp[i]!=-1){
-            return dp[i] == 1;
+        if(dp[i]!=null){
+            return dp[i];
         }
+
         for(int j=1;j<=nums[i];j++){
-           int next = i+j;
-           if(next < nums.length && helper(nums , next ,dp)){
-            dp[i] =1;
-            return true;
-           }
-        } 
-        dp[i] = 0;
-        return false;
+            int next = i+j;
+            if(helper(nums,next,dp)==true){
+                return dp[i] = true;
+            }
+        }
+
+        return dp[i] = false;
     }
+
+    
 }
